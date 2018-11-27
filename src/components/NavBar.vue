0db24d50c411e5a6a5cfcd653e02aa297956dbdf
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-11-27 18:42:33 +0100

    Zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+       
+           <router-link to="/cars">Cars</router-link> 
+        
+            <a class="nav-link" href="#">Features</a>
+        
+            <a class="nav-link" href="#">Pricing</a>
+        
+            <a class="nav-link" href="#">Disabled</a>
+      
+        </nav>
+</div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', redirect: '/cars' },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
