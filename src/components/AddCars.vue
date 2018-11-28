d4cc9b3d1945e5145a25105a5b0e0f97bc7e0aff
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-11-28 02:38:57 +0100

    Zadatak 6

--- src/components/AddCars.vue
+++ src/components/AddCars.vue
@@ -1,45 +1,53 @@
 <template>
     <div class="container">
-        <form @submit.prevent="addCar" ref="form">
-            <div class="form-group">
-                <input type="text" name="file" class="form-control" placeholder="Brand" v-model="newCar.brand">
-            </div>
-            <div class="form-group">
-                <input type="text" name="file" class="form-control" placeholder="Model" v-model="newCar.model">
-            </div>
-             <div class="form-group">
-                <input type="number" name="file" class="form-control" placeholder="Max Speed" v-model="newCar.maxSpeed">
-            </div>
-             <div class="form-group">
-                <input type="number"  class="form-control" placeholder="Number Of Doors" v-model="newCar.numberOfDoors" name="file">
-            </div>
-            <select  aria-placeholder="Select Section" v-model="newCar.year" :value="years" name="file" >
-                <option disabled value="">Select year</option>
-                <option v-for="(year, index) in years" :key="index">
-                    {{ year }}
-                </option>
-            </select>
-            <div class="custom-control ">
-                <input type="checkbox" v-model="newCar.isAutomatic" name="file" >
-                <label class="custom-controll">Automatic</label>
-            </div>
-        
-             <div class="engine-input">
-                 <label>Disel</label>
-                <input type="radio" v-model="newCar.engine" value="Disel" name="file">
-                <label>Petrol</label>
-                <input type="radio" v-model="newCar.engine" value="Petrol" name="file">
-                <label>Electric</label>
-                <input type="radio" v-model="newCar.engine" value="Electric" name="file">
-                <label>Hybrid</label>
-                <input type="radio" v-model="newCar.engine" value="Hybrid" name="file">
-            </div>
-           
-            <button type="submit" class="btn btn-primary">Add car</button>
-            <button type="submit" class="btn btn-info" @click.stop.prevent="previewCar">Preview</button>
-            <button  class="btn btn-danger" @click.stop.prevent="resetForm">Reset</button>
-        </form>
-         
+        <div class="container mt-3 mt-sm-5">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form @submit.prevent="addCar" ref="form">
+                        <div class="form-group">
+                            <input type="text"  class="form-control" required="required" minlength="2" placeholder="Brand" v-model="newCar.brand">
+                        </div>
+                        <div class="form-group">
+                            <input type="text"  class="form-control" placeholder="Model" required="required" minlength="2" v-model="newCar.model">
+                        </div>
+                        <div class="form-group">
+                            <input type="number" class="form-control" placeholder="Max Speed" v-model="newCar.maxSpeed">
+                        </div>
+                        <div class="form-group">
+                            <input type="number"  class="form-control" placeholder="Number Of Doors" required="required" v-model="newCar.numberOfDoors">
+                        </div>
+                        <div class="select">
+                            <select  aria-placeholder="Select Section" v-model="newCar.year" :value="years" required="required" >
+                                <option disabled value="">Select year</option>
+                                <option v-for="(year, index) in years" :key="index">
+                                    {{ year }}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="custom-control ">
+                            <input type="checkbox" v-model="newCar.isAutomatic" >
+                            <label class="custom-controll">Automatic</label>
+                        </div>
+                        <div class="validate-radio">
+                            <div class="engine-input">
+                                <label>Disel</label>
+                                <input type="radio" v-model="newCar.engine" value="Disel" required="required">
+                                <label>Petrol</label>
+                                <input type="radio" v-model="newCar.engine" value="Petrol" required="required">
+                                <label>Electric</label>
+                                <input type="radio" v-model="newCar.engine" value="Electric" required="required">
+                                <label>Hybrid</label>
+                                <input type="radio" v-model="newCar.engine" value="Hybrid" required="required">
+                            </div>
+                        </div>
+                    
+                        <button type="submit" class="btn btn-primary">Add car</button>
+                        <button type="submit" class="btn btn-info" @click.stop.prevent="previewCar">Preview</button>
+                        <button  class="btn btn-danger" @click.stop.prevent="resetForm">Reset</button>
+                    </form>
+                </div><!-- /col -->
+            </div><!-- /row -->
+        </div><!-- /container -->  
     </div>
 </template>
 <script>
@@ -50,16 +58,14 @@ export default {
            years: Array(29).fill(1990).map((n, i) => n + i),
            newCar: {
                isAutomatic: false
-           },
-           file: ''
-        
+           }
         }
     },
     methods: {
         addCar() {
             CarsService.add(this.newCar);
-            this.newCar = {}
-            this.$router.push({ path: '/cars' })
+            this.newCar = {};
+            this.$router.push({ path: '/cars' });
         },
         resetForm() {
             this.newCar = {}
@@ -67,7 +73,7 @@ export default {
         previewCar() {
            let stringForm = JSON.stringify(this.newCar);
            let parseString = JSON.parse(stringForm);
-           alert(`Brand:${ parseString.brand }\nModel:${ parseString.model }\nMax Speed:${ parseString.maxSpeed }\n Number Of Doors:${ parseString.numberOfDoors }\n Year:${ parseString.year }\nAutomatic:${ parseString.isAutomatic }\n Engine:${ parseString.engine }`);
+           alert(`Brand:${ parseString.brand }\nModel:${ parseString.model }\nMax Speed:${ parseString.maxSpeed }\n Number Of Doors:${ parseString.numberOfDoors }\n Year:${ parseString.year }\nAutomatic:${ parseString.isAutomatic ? 'Automatic' : 'Manual'}\n Engine:${ parseString.engine }`);
         }
     }
 }
